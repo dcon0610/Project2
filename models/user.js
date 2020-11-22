409deda69e6711d8192971beb51131012bfb8a5e
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-11-22 13:11:49 +1030

    remove unnessary files, attemp to fix sequelize (fail)

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../config/connection.js");
+// const Sequelize = require("sequelize");
+// const sequelize = require("../config/connection.js");
 
-var User = sequelize.define("users", {
-    user_name: Sequelize.STRING
-  }, {
-    freezeTableName: true
-  });
+// var User = sequelize.define("users", {
+//     user_name: Sequelize.STRING
+//   }, {
+//     freezeTableName: true
+//   });
 
-  // Syncs with DB
-  User.sync();
+//   // Syncs with DB
+//   User.sync();
   
-  // Makes the Coffee Model available for other files (will also create a table)
-  module.exports = User;
+//   // Makes the Coffee Model available for other files (will also create a table)
+//   module.exports = User;
