3122010bae4cba4486d369718676a3df96d309c8
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-11-23 12:51:30 +1030

    reinstalled sequelize and components

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
-const express = require("express");
-const db = require("./models/index.js");
+var express= require("express");
+var db = require("./models");
 
-var PORT = process.env.PORT || 3001;
-
+var PORT = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static("public"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// set handlebars
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-
-var routes = require("./controllers/controller.js");
-
-
-app.use(routes);
-
-<<<<<<< HEAD
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log("listening on port %s", PORT);
+    });
 });
-
-=======
-
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App now listening at localhost:" + PORT);
-  });
-});
->>>>>>> main
