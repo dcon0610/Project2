31d0a38bf4b489d57d9f411d17ddfe80f7962d92
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-11-23 12:54:52 +1030

    added back in handlebars and router stuff to server js

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ var db = require("./models");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/controller.js");
+
+app.use(routes);
+
+
 db.sequelize.sync().then(function(){
     app.listen(PORT, function(){
         console.log("listening on port %s", PORT);
